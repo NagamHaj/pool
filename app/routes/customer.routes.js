d0cd6a2b62ce4a0252512498858b3b5fff2c6b1e
Nagam <Nagam94@gmail.com>
2020-06-05 12:00:54 +0300

    commit

--- app/routes/customer.routes.js
+++ app/routes/customer.routes.js
@@ -1,10 +1,11 @@
 module.exports = app => {
-  const customers = require("../controllers/customer.controller.js");
+ // const customers = require("../controllers/customer.controller.js");
+  
 
  
 
   // Retrieve all Customers
-  app.get("/customers", require('./routes/api/users'));
+  app.get("/customers", require('./models/customer.model'));
 
 
 };
